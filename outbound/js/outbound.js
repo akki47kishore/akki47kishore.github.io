71acbbd012263a2b4f22cbd90e503dc9cdda5afa
akshaykishore <akshaykishore@qburst.com>
2018-10-10 10:47:39 +0530

    Removing unwanted code

--- outbound/js/outbound.js
+++ outbound/js/outbound.js
@@ -51,17 +51,6 @@ function addNewOutbound() {
             }
         }
     }
-    // for (let itemCategory in inboundOutboundFormat.supplies) {
-    //     for (let itemsubCategory in inboundOutboundFormat.supplies[itemCategory]) {
-    //         if (typeof (inboundOutboundFormat.supplies[itemCategory][itemsubCategory]) == "object") {
-    //             for (let value in inboundOutboundFormat.supplies[itemCategory][itemsubCategory]) {
-    //                 currentStock[itemCategory][itemsubCategory][value] -= inboundOutboundFormat.supplies[itemCategory][itemsubCategory][value];
-    //             }
-    //         } else {
-    //             currentStock[itemCategory][itemsubCategory] -= inboundOutboundFormat.supplies[itemCategory][itemsubCategory];
-    //         }
-    //     }
-    // }
     initialStock.forEach(element => {
         if(element[3]){
             currentStock[element[2]][element[1]][element[0]] -= inboundOutboundFormat.supplies[element[2]][element[1]][element[0]];
